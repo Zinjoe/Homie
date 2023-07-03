8d4d6f7f74b13656f7b1b231f96836faa6c46840
joeLin <a29482992@gmail.com>
2023-07-03 14:47:29 +0800

    create maven project

---
+++ src/main/java/test/controller/TestController.java
@@ -0,0 +1,28 @@
+package test.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	   
+		
+			String name =request.getParameter("name");
+			
+			request.setAttribute("nickname", name);
+			
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			
+			
+	}
+
+}
